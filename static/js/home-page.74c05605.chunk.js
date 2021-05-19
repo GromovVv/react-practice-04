4e1c9b435be6c2088849f8a37de251a93bb48164
GromovVv <pavel.kuts@gmail.com>
2021-05-19 04:00:08 +0300

    Updates

---
+++ static/js/home-page.74c05605.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-practice-04"]=this["webpackJsonpreact-practice-04"]||[]).push([[4],{72:function(t,c,e){"use strict";e.r(c),e.d(c,"default",(function(){return n}));var r=e(1),n=function(){return Object(r.jsx)("h1",{style:{backgroundColor:"skyblue"},children:"This is HOME PAGE"})}}}]);
+//# sourceMappingURL=home-page.74c05605.chunk.js.map
